7d050c56f8c6a6ba0da8f1e717d9354ba4fc926f
abhishek.sangani <abhishek.sangani@dakshyafreondtech.com>
2023-08-09 13:32:04 +0530

    TB-3 Implemented CloverRestCategoryAPI getAllCategories method

--- src/main/java/io/github/dftrakesh/cloverrest/model/inventoery/category/CategoriesResponse.java
+++ src/main/java/io/github/dftrakesh/cloverrest/model/inventoery/category/CategoriesResponse.java
@@ -1,15 +1,14 @@
 package io.github.dftrakesh.cloverrest.model.inventoery.category;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.github.dftrakesh.cloverrest.model.inventoery.item.Items;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CategoryElement {
+public class CategoriesResponse {
 
-    private String id;
-    private String name;
-    private Integer sortOrder;
-    private Items items;
+    private List<CategoryElement> elements;
+    private String href;
 }
